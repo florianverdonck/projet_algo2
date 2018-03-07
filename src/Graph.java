27d05b7bdc51702d27c8843a8805312a05e01d0b
Florian <florianverdonck@gmail.com>
2018-03-07 09:15:47 +0100

    FILES

---
+++ src/Graph.java
@@ -0,0 +1,13 @@
+
+public class Graph {
+
+	public void calculerItineraireMiniminantDistance(String string, String string2, String string3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void calculerItineraireMinimisantNombreVol(String string, String string2, String string3) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/SAXHandler.java
@@ -0,0 +1,11 @@
+import org.xml.sax.HandlerBase;
+
+@SuppressWarnings("deprecation")
+public class SAXHandler extends HandlerBase {
+
+	public Graph getGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
