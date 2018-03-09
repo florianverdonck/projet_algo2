877b87956dafb0f9ba9de473a28383c44e3a7895
Ekran97 <37137141+Ekran97@users.noreply.github.com>
2018-03-09 15:38:27 +0100

    add company

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -7,11 +9,13 @@ public class Graph {
 
   Set<Vol> vols;
   Map<String, Aeroport> aeroports;
+  Map<String, Compagnie> compagnies;
 
   public Graph() {
     super();
     vols = new HashSet<>();
     aeroports = new HashMap<>();
+    compagnies = new HashMap<>();
   }
 
   public void ajouterVol(Vol vol) {
@@ -34,14 +38,45 @@ public class Graph {
       definitif.put(nom, null);
     }
 
-    aeroports.get(source).getOut();
+    for (Vol vol : aeroports.get(source).getOut()) {
+
+    }
 
 
 
   }
 
   public void calculerItineraireMinimisantNombreVol(String source, String destination,
-      String fichier) {
+      String fichier) throws PasDeVolException {
+    Deque<Aeroport> file = new ArrayDeque<>();
+    Set<Aeroport> visites = new HashSet<>();
+    Map<Aeroport, Vol> chemins = new HashMap<>();
+    Vol[] vols;
+
+    Aeroport aSource = aeroports.get(source);
+    Aeroport aDest;
+
+    file.push(aSource);
+    chemins.put(aSource);
+
+    for (Aeroport a : file) {
+      for (Vol vol : a.getOut()) {
+        aDest = vol.getDestination();
+        if (aDest.getNom().equals(destination)) {
+
+          break;
+        }
+        if (!visites.contains(aDest)) {
+          file.addLast(aDest);
+          visites.add(aDest);
+          chemins.put(aDest, vol);
+        }
+      }
+      file.removeFirst();
+    }
+
+    if (file.isEmpty())
+      throw new PasDeVolException();
 
 
 
---
+++ src/PasDeVolException.java
@@ -0,0 +1,4 @@
+
+public class PasDeVolException extends Exception {
+
+}
